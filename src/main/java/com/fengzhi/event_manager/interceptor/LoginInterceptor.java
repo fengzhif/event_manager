787cd47235917bee8be1944329d1b668d14676f3
fengzhif <1967301946@qq.com>
2025-04-01 18:25:04 +0800

    add interceptor for authorization

---
+++ src/main/java/com/fengzhi/event_manager/interceptor/LoginInterceptor.java
@@ -0,0 +1,35 @@
+package com.fengzhi.event_manager.interceptor;
+
+import com.fengzhi.event_manager.utils.JwtUtil;
+import com.fengzhi.event_manager.utils.ThreadLocalUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.util.Map;
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        System.out.println(token);
+        try{
+            Map<String,Object> claim= JwtUtil.parseToken(token);
+            System.out.println(claim);
+            ThreadLocalUtil.set(claim);
+            return true;
+        }
+        catch(Exception e){
+            response.setStatus(401);
+            return false;
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        ThreadLocalUtil.remove();
+    }
+}
