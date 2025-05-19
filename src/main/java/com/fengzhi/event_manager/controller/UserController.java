87bab0a16c978858bd8fedb0c5c1c8eccbb7beb5
fengzhif <1967301946@qq.com>
2025-05-19 16:14:42 +0800

    add redis

--- src/main/java/com/fengzhi/event_manager/controller/UserController.java
+++ src/main/java/com/fengzhi/event_manager/controller/UserController.java
@@ -1,20 +1,22 @@
 package com.fengzhi.event_manager.controller;
 
-import com.auth0.jwt.JWT;
 import com.fengzhi.event_manager.pojo.Result;
 import com.fengzhi.event_manager.pojo.User;
 import com.fengzhi.event_manager.service.impl.UserService;
 import com.fengzhi.event_manager.utils.JwtUtil;
 import com.fengzhi.event_manager.utils.Md5Util;
 import com.fengzhi.event_manager.utils.ThreadLocalUtil;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
-import jakarta.validation.constraints.Pattern;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -22,10 +24,11 @@ import java.util.Map;
 public class UserController {
 
     UserService userService;
-
+    StringRedisTemplate stringRedisTemplate;
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, StringRedisTemplate stringRedisTemplate) {
         this.userService = userService;
+        this.stringRedisTemplate = stringRedisTemplate;
     }
 
     @PostMapping("/register")
@@ -51,6 +54,8 @@ public class UserController {
                 claims.put("username", username);
                 claims.put("id", user.getId());
                 String token = JwtUtil.generateToken(claims);
+                //token存入Redis
+                stringRedisTemplate.opsForValue().set(token, token,1, TimeUnit.HOURS);
                 return Result.success(token);
             } else {
                 return Result.error("密码错误");
@@ -81,7 +86,7 @@ public class UserController {
     }
 
     @PatchMapping("/updatePwd")
-    public Result updatePwd(@RequestBody Map<String, String> params) {
+    public Result updatePwd(@RequestBody Map<String, String> params, @RequestHeader("Authorization") String token) {
         String old_pwd = params.get("old_pwd");
         String new_pwd = params.get("new_pwd");
         String re_pwd = params.get("re_pwd");
@@ -111,6 +116,8 @@ public class UserController {
         }
 
         userService.updatePwd(id, Md5Util.getMD5String(new_pwd));
+        ValueOperations<String, String> operations = stringRedisTemplate.opsForValue();
+        operations.getOperations().delete(token);
         return Result.success();
     }
 }
--- src/main/java/com/fengzhi/event_manager/interceptor/LoginInterceptor.java
+++ src/main/java/com/fengzhi/event_manager/interceptor/LoginInterceptor.java
@@ -4,6 +4,8 @@ import com.fengzhi.event_manager.utils.JwtUtil;
 import com.fengzhi.event_manager.utils.ThreadLocalUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -11,12 +13,21 @@ import java.util.Map;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
+    StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
 
     @Override
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
         String token = request.getHeader("Authorization");
         try {
             Map<String, Object> claim = JwtUtil.parseToken(token);
+            String redisToken = stringRedisTemplate.opsForValue().get(token);
+            if(redisToken == null) {
+                throw new RuntimeException();
+            }
             ThreadLocalUtil.set(claim);
             return true;
         } catch (Exception e) {
